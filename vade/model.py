51158cb44e2ace4a8a0ce4da6148601e424faa1d
Taro Makino <taro@nyu.edu>
2023-02-03 13:50:52 -0500

    Remove softplus

--- vade/model.py
+++ vade/model.py
@@ -59,9 +59,9 @@ class Model(pl.LightningModule):
         self.lr = lr
 
 
-    def sample_z(self, mu, var):
+    def sample_z(self, mu, logvar):
         if self.training:
-            sd = var.sqrt()
+            sd = torch.exp(logvar / 2) # Same as sqrt(exp(logvar))
             eps = torch.randn_like(sd)
             return mu + eps * sd
         else:
@@ -71,8 +71,7 @@ class Model(pl.LightningModule):
     def forward(self, x):
         # z ~ q(z|x)
         mu_tilde, logvar_tilde = self.q_z_x_net(x)
-        var_tilde = F.softplus(logvar_tilde)
-        z = self.sample_z(mu_tilde, var_tilde)
+        z = self.sample_z(mu_tilde, logvar_tilde)
         # E_q(z,c|x)[log p(x|z)]
         mu_x = torch.sigmoid(self.p_x_z_net(z))
         log_p_x_z = (x * torch.log(mu_x) + (1 - x) * torch.log(1 - mu_x)).sum(-1).mean()
@@ -80,7 +79,7 @@ class Model(pl.LightningModule):
         p_c = distributions.Categorical(logits=self.logits_c)
         p_z_c = distributions.Independent(distributions.Normal(loc=self.mu_z_c, scale=torch.exp(0.5 * self.logvar_z_c)), 1)
         p_z = MixtureSameFamily(p_c, p_z_c)
-        log_q_z_x = diagonal_mvn_log_prob(z, mu_tilde, var_tilde, self.device)
+        log_q_z_x = diagonal_mvn_log_prob(z, mu_tilde, logvar_tilde, self.device)
         kl = (log_q_z_x - p_z.log_prob(z)).mean()
         elbo = log_p_x_z - kl
         return {
@@ -92,6 +91,8 @@ class Model(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y = batch
         out = self.forward(x)
+        self.log("train_loss", out["loss"], on_step=False, on_epoch=True)
+        self.log("train_kl", out["kl"], on_step=False, on_epoch=True)
         return out["loss"]
 
 
--- vade/utils/stats.py
+++ vade/utils/stats.py
@@ -1,5 +1,5 @@
 import torch
 
-def diagonal_mvn_log_prob(x, mu, var, device):
+def diagonal_mvn_log_prob(x, mu, logvar, device):
     c = 2 * torch.pi * torch.ones(1).to(device)
-    return (-0.5 * (torch.log(c) + var.log() + (x - mu).pow(2).div(var))).sum(dim=-1)
+    return (-0.5 * (torch.log(c) + logvar + (x - mu).pow(2).div(torch.exp(logvar)))).sum(dim=-1)
